1ee4a5fcdc88dae0c787079e4294ca4f1f8029b4
Iasmim Cardoso <iasmimcardoso11@gmail.com>
2024-01-10 10:44:10 -0300

    Criando Base do projeto do banco digital

---
+++ src/main/java/com/iasarchi/projetobancodigital/ProjetoBancoDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.iasarchi.projetobancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBancoDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoBancoDigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iasarchi/projetobancodigital/ProjetoBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iasarchi.projetobancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancoDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
